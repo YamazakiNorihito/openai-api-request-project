43b140a5eae4b04c2754bfcaae1cd9952418b57c
Yamazaki Norihito <kizinn1224@gmail.com>
2023-05-15 22:43:49 +0900

    OpenAi APIのService作成

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,25 @@
+using System.Net.Http.Headers;
 using Client;
+using Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddHttpClient();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+
+builder.Services.AddSingleton<IOpenAIService, OpenAIService>(
+    p =>
+    {
+        var config = p.GetRequiredService<IConfiguration>();
+        var httpClientFactory = p.GetRequiredService<IHttpClientFactory>();
+        return new OpenAIService(httpClientFactory, config["APIkey"]);
+    }
+);
 await builder.Build().RunAsync();
---
+++ Client/Services/OpenAIService.cs
@@ -0,0 +1,78 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace Client.Services;
+
+public interface IOpenAIService
+{
+    Task<string> GetCompletionAsync(string prompt);
+}
+
+public class OpenAIService : IOpenAIService
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly string _token;
+
+    public OpenAIService(IHttpClientFactory httpClientFactory, string token)
+    {
+        (_httpClientFactory, _token) = (httpClientFactory, token);
+    }
+
+    public async Task<string> GetCompletionAsync(string prompt)
+    {
+        var requestData = new
+        {
+            model = "text-davinci-003",
+            prompt = prompt,
+            max_tokens = 50,
+            temperature = 0.6
+        };
+        var jsonRequestData = JsonSerializer.Serialize(requestData);
+        var httpRequestMessage = new HttpRequestMessage(
+            HttpMethod.Post,
+            "https://api.openai.com/v1/completions")
+        {
+            Content = new StringContent(jsonRequestData, Encoding.UTF8, "application/json"),
+            Headers =
+            {
+                { "Authorization", $"Bearer {_token}" },
+            }
+        };
+
+        var httpClient = _httpClientFactory.CreateClient("OpenAIApi");
+        using var response = await httpClient.SendAsync(httpRequestMessage, HttpCompletionOption.ResponseContentRead);
+        var responseContent = await response.Content.ReadAsStringAsync();
+        var responseObject = JsonSerializer.Deserialize<TextCompletionResponse>(responseContent,
+            new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+        return responseObject!.Choices[0].Text;
+    }
+}
+
+public class TextCompletionResponse
+{
+    public string Id { get; set; }
+    public string Object { get; set; }
+    public long Created { get; set; }
+    public string Model { get; set; }
+    public List<Choice> Choices { get; set; }
+    public Usage Usage { get; set; }
+}
+
+public class Choice
+{
+    public string Text { get; set; }
+    public int Index { get; set; }
+    public object Logprobs { get; set; }
+    public string FinishReason { get; set; }
+}
+
+public class Usage
+{
+    public int PromptTokens { get; set; }
+    public int CompletionTokens { get; set; }
+    public int TotalTokens { get; set; }
+}
